f0e795d6986d292ab4bf555ba691734815f7bf53
JialuJialu <baojialuhz@gmail.com>
2017-08-14 09:34:55 -0400

    delete redundant steps

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ textstat
 
 Python package to calculate statistics from text, which helps to decide readability, complexity and grade level of a particular corpus.
 
+(Still Testing)
+-------
+
+Difference between this Fork and its Master
+-------
+In short, this version greatly optimized the time efficiency, especially in the case that the user need several readability scores of a test text. Consequently, it requires a change in the usage.
+ 
+Though there are various readability scores (also called textstat), they are all based on a limited set of measurements of the text, i.e. average sentence length, average syllable count per word, average character per word, etc. In the original version at the master, these results are calculated repeatedly when the user asks for more than one readability scores of a certain text. This version of implemetation calculate them all once and pass them to readability score functions wanted by the user. 
+
+This version also gets rid of some other repetitive calculations and implements syllable count differently. The master version is using the number of possible hyphenizations as the number of syllables in a word, but at least for the project I am working on, it makes more sense to use vowel-based syllable count, so I change it.
+
 
 Install
 -------
@@ -36,10 +47,17 @@ You can install it by doing the following,:
    
     $ python setup.py install # as root
 ```
-
 List of Functions
 ----
+### Character Count
 
+function name - char_count(txt, ignorespaces=True):
+
+Function to return total character counts in a text, pass the following parameter
+
+    ignorespaces = False
+to ignore whitespaces
+    
 ### Syllable Count
 
 function name - syllable_count(text)
@@ -48,7 +66,11 @@ returns - the number of syllables present in the given text.
 
 ### Lexicon Count
 
-function name - lexicon_count(text, TRUE/FALSE)
+function name - lexicon_count(wordlist, TRUE/FALSE)
+
+wordlist is supposed to be the the list of all words in the test text. It can be obtained by 
+
+	wordlist = [ch for ch in txt if ch not in exclude]
 
 Calculates the number of words present in the text.
 TRUE/FALSE specifies whether we need to take in account in punctuation symbols while counting lexicons or not.
@@ -60,61 +82,36 @@ function name - sentence_count(text)
 
 returns the number of sentences present in the given text.
 
+### Readability Scores
 
-### The Flesch Reading Ease formula
+function name - readability_scores(text,defaultscores)
 
-function name - flesch_reading_ease(text)
+If not specified
 
-returns the Flesch Reading Ease Score. Following table is helpful to access the ease of readability in a document.
+	defaultscores = ["flesch_ease","smog","flesch_grade","coleman_liau","automated",
+          "dale_chall","linsear_write","lix","gunning_fog"]
 
-* 90-100 : Very Easy 
-* 80-89 : Easy 
-* 70-79 : Fairly Easy 
-* 60-69 : Standard 
-* 50-59 : Fairly Difficult 
-* 30-49 : Difficult 
-* 0-29 : Very Confusing
+returns a dictionary of requested scores:
 
-### The The Flesch-Kincaid Grade Level
+	example_return_dict = {"flesch_ease":10,"smog":10,"flesch_grade":10,"coleman_liau":10,"automated":10,
+          "dale_chall":10,"linsear_write":10,"lix":10,"gunning_fog":10}
 
-function name - flesch_kincaid_grade(text)
+### Average Sentence Length
+function name - avg\_sentence\_length (lexicon count,sentence count)
 
-returns the grade score using the Flesch-Kincaid Grade Formula.
-</n>For example a score of 9.3 means that a ninth grader would be able to read the document.
+### Average Syllables per word
 
-## The Fog Scale (Gunning FOG Formula)
-function name - gunning_fog(text)
-returns the FOG index of the given text.
+function name - avg\_syllables\_per_word(syllable_count,lexicon_count)
 
-### The SMOG Index
-function name - smog_index(text)
+### Average Letters Per word
 
-return the SMOG index of the given text.
+function name - avg\_letter\_per\_word(character count,lexicon count)
 
-### Automated Readability Index
-function name - automated_readability_index(text)
+### Polysyllble words count
 
-returns the ARI(Automated Readability Index) which outputs a number that approximates the grade level needed to comprehend the text.
-</n>For example if the ARI is 6.5, then the grade level to comprehend the text is 6th to 7th grade.
+function name - polysyllabcount(wordlist)
 
-### The Coleman-Liau Index
-function name - coleman_liau_index(text)
-
-returns the grade level of the text using the Coleman-Liau Formula
-
-### Linsear Write Formula
-function name - linsear_write_formula(text)
-
-returns the grade level using the Lisear Write Formula
-
-### Dale-Chall Readability Score
-function name - dale_chall_readability_score(text)
-
-Different from other tests, since it uses a lookup table of most commonly used 3000 english words.
-Thus it returns the grade level using the New Dale-Chall Formula.
-### Readability Consensus based upon all the above tests
-function name - text_standard(text)
-Based upon all the above tests returns the best grade level under which the given text belongs to.
+returns the number of words that have more than two syllables
 
 
 Usage
@@ -124,22 +121,9 @@ from textstat.textstat import textstat
 if __name__ == '__main__':
 		test_data = """Playing games has always been thought to be important to the development of well-balanced and creative children; however, what part, if any, they should play in the lives of adults has never been researched that deeply. I believe that playing games is every bit as important for adults as for children. Not only is taking time out to play games with our children and other adults valuable to building interpersonal relationships but is also a wonderful way to release built up tension."""
 
-	print textstat.flesch_reading_ease(test_data)
-	print textstat.smog_index(test_data)
-	print textstat.flesch_kincaid_grade(test_data)	print textstat.coleman_liau_index(test_data)
-	print textstat.automated_readability_index(test_data)
-	print textstat.dale_chall_readability_score(test_data)
-	print textstat.difficult_words(test_data)
-	print textstat.linsear_write_formula(test_data)
-	print textstat.gunning_fog(test_data)
-	print textstat.text_standard(test_data)
+	print readability_scores(test_data,list_of_scores_wanted)
+	
 ```
-
-the arguement (text) for all the functions defined remains same - 
-i.e the text for which statistics needs to be calculated
-
-
-
 
 
 
--- textstat/textstat.py
+++ textstat/textstat.py
@@ -1,296 +1,233 @@
 from __future__ import print_function
 import pkg_resources
 import string
-import re
 import math
 import operator
-from pyphen import Pyphen
+
+_all_ = [readability_scores,list_implemented_scores,char_count,syllable_count,sentence_count,avg_sentence_length,avg_syllables_per_word,avg_letter_per_word,polysyllabcount]
 
 exclude = list(string.punctuation)
 easy_word_set = set([ln.strip() for ln in pkg_resources.resource_stream('textstat', 'easy_words.txt')])
 
+def char_count(txt, ignore_spaces=True):
+    """
+    Function to return total character counts in a text, pass the following parameter
+    ignore_spaces = False
+    to ignore whitespaces
+    """
+    if ignore_spaces:
+        text = txt.replace(" ", "")
+        return len(text)
+    else:
+        return len(txt)
+    
 
-class textstatistics:
-    def __init__(self):
+def syllable_count(txt_given):
+    """
+    Function to calculate syllable words in a text.
+    I/P - a text
+    O/P - number of syllable words
+    """
+    count = 0
+    vowels = 'aeiou'
+    extendedvowels = 'aeiouy'
+    text = txt_given.lower()
+    text = "".join(x for x in text if x not in exclude)
+
+    if text is None:
+        return 0
+    elif len(text) == 0:
+        return 0
+    else:
+        if text[0] in vowels:
+            count += 1
+        for index in range(1, len(text)):
+            if text[index] in extendedvowels and text[index-1] not in vowels:
+                count += 1
+#             if text.endswith('e') and (text[-2] != "l" or (text[-3] in vowels)):
+        if text.endswith('e') and (not text.endswith("le")):
+            #discard trailing "e", except where ending is "le", but include words like pale, male, mole
+            count -= 1
+        if count == 0:
+            count += 1
+        return count
+
+def sentence_count(txt):
+    """
+    Sentence count of a text
+    """
+    ignoreCount = 0
+    sentences = re.split(r' *[\.\?!][\'"\)\]]* *', txt)
+    for sentence in sentences:
+        if lexicon_count(sentence) <= 2:
+            ignoreCount = ignoreCount + 1
+    return max(1, len(sentences) - ignoreCount)
+
+    
+def avg_sentence_length(lc,sc):
+    try:
+        ASL = float(lc)/sc
+        return round(ASL, 2)
+    except:
+        print("Error(ASL): Sentence Count is Zero, Cannot Divide")
+        return
+
+def avg_syllables_per_word(syllable_c,lc):
+    try:
+        ASPW = float(syllable_c)/float(lc)
+        return round(ASPW, 2)
+    except:
+        print("Error(ASyPW): Number of words are zero, cannot divide")
+        return
+
+def avg_letter_per_word(char_c,lc):
+    try:
+        ALPW = float(char_c)/float(lc)
+        return round(ALPW, 2)
+    except:
+        print("Error(ALPW): Number of words are zero, cannot divide")
+        return
+
+    
+def polysyllabcount(wordlist):
+    count = 0
+    for word in wordlist:
+        wrds = syllable_count(word)
+        if wrds >= 3:
+            count += 1
+    return count
+
+    
+def flesch_reading_ease(ASL,ASW):
+    FRE = 206.835 - float(1.015 * ASL) - float(84.6 * ASW)
+    return round(FRE, 2)
+
+def flesch_kincaid_grade(ASL,ASW):
+    FKRA = float(0.39 * ASL) + float(11.8 * ASW) - 15.59
+    return round(FKRA, 2)
+
+
+def smog_index(poly_syllab,sc):
+    if sc >= 3:
+        try:
+            SMOG = (1.043 * (30*(float(poly_syllab)/sc))**.5) + 3.1291
+            return round(SMOG, 2)
+        except:
+            print("Error(SI): Sentence count is zero, cannot divide")
+    else:
+        return 0
+
+def coleman_liau_index(ALW, ASL):
+    CLI = (0.058 * ALW *100) - (29.6 /float(ASL)) - 15.8
+    return round(CLI, 2)
+
+def automated_readability_index(char_c,lc,sc):
+    try:
+        a = float(char_c)/lc
+        b = float(char_c)/sc
+        ARI = (4.71 * round(a, 2)) + (0.5*round(b, 2)) - 21.43
+        return round(ARI, 2)
+    except Exception as E:
+        print("Error(ARI) : Sentence count is zero, cannot divide")
         return None
 
-    def char_count(self, text, ignore_spaces=True):
-        """
-        Function to return total character counts in a text, pass the following parameter
-        ignore_spaces = False
-        to ignore whitespaces
-        """
-        if ignore_spaces:
-            text = text.replace(" ", "")
-        return len(text)
+def linsear_write_formula(wordlist,sc,lc):
+    easy_word = []
+    difficult_word = []
 
-    def lexicon_count(self, text, removepunct=True):
-        """
-        Function to return total lexicon (words in lay terms) counts in a text
-        """
-        if removepunct:
-            text = ''.join(ch for ch in text if ch not in exclude)
-        count = len(text.split())
-        return count
-
-    def syllable_count(self, text, lang='en_US'):
-        """
-        Function to calculate syllable words in a text.
-        I/P - a text
-        O/P - number of syllable words
-        """
-        text = text.lower()
-        text = "".join(x for x in text if x not in exclude)
-
-        if text is None:
-            return 0
-        elif len(text) == 0:
-            return 0
-        else:
-            dic = Pyphen(lang=lang)
-            count = 0
-            for word in text.split(' '):
-                word_hyphenated = dic.inserted(word)
-                count += max(1, word_hyphenated.count("-") + 1)
-            return count
-
-    def sentence_count(self, text):
-        """
-        Sentence count of a text
-        """
-        ignoreCount = 0
-        sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
-        for sentence in sentences:
-            if self.lexicon_count(sentence) <= 2:
-                ignoreCount = ignoreCount + 1
-        return max(1, len(sentences) - ignoreCount)
-
-    def avg_sentence_length(self, text):
-        lc = self.lexicon_count(text)
-        sc = self.sentence_count(text)
+    Number = 0
+    for value in wordlist:
         try:
-            ASL = float(lc/sc)
-            return round(lc/sc, 1)
-        except:
-            print("Error(ASL): Sentence Count is Zero, Cannot Divide")
-            return
-
-    def avg_syllables_per_word(self, text):
-        syllable = self.syllable_count(text)
-        words = self.lexicon_count(text)
-        try:
-            ASPW = float(syllable)/float(words)
-            return round(ASPW, 1)
-        except:
-            print("Error(ASyPW): Number of words are zero, cannot divide")
-            return
-
-    def avg_letter_per_word(self, text):
-        try:
-            ALPW = float(float(self.char_count(text))/float(self.lexicon_count(text)))
-            return round(ALPW, 2)
-        except:
-            print("Error(ALPW): Number of words are zero, cannot divide")
-            return
-
-    def avg_sentence_per_word(self, text):
-        try:
-            ASPW = float(float(self.sentence_count(text))/float(self.lexicon_count(text)))
-            return round(ASPW, 2)
-        except:
-            print("Error(AStPW): Number of words are zero, cannot divide")
-            return
-
-    def flesch_reading_ease(self, text):
-        ASL = self.avg_sentence_length(text)
-        ASW = self.avg_syllables_per_word(text)
-        FRE = 206.835 - float(1.015 * ASL) - float(84.6 * ASW)
-        return round(FRE, 2)
-
-    def flesch_kincaid_grade(self, text):
-        ASL = self.avg_sentence_length(text)
-        ASW = self.avg_syllables_per_word(text)
-        FKRA = float(0.39 * ASL) + float(11.8 * ASW) - 15.59
-        return round(FKRA, 1)
-
-    def polysyllabcount(self, text):
-        count = 0
-        for word in text.split():
-            wrds = self.syllable_count(word)
-            if wrds >= 3:
-                count += 1
-        return count
-
-    def smog_index(self, text):
-        if self.sentence_count(text) >= 3:
-            try:
-                poly_syllab = self.polysyllabcount(text)
-                SMOG = (1.043 * (30*(poly_syllab/self.sentence_count(text)))**.5) + 3.1291
-                return round(SMOG, 1)
-            except:
-                print("Error(SI): Sentence count is zero, cannot divide")
-        else:
-            return 0
-
-    def coleman_liau_index(self, text):
-        L = round(self.avg_letter_per_word(text)*100, 2)
-        S = round(self.avg_sentence_per_word(text)*100, 2)
-        CLI = float((0.058 * L) - (0.296 * S) - 15.8)
-        return round(CLI, 2)
-
-    def automated_readability_index(self, text):
-        chrs = self.char_count(text)
-        wrds = self.lexicon_count(text)
-        snts = self.sentence_count(text)
-        try:
-            a = (float(chrs)/float(wrds))
-            b = (float(wrds)/float(snts))
-            ARI = (4.71 * round(a, 2)) + (0.5*round(b, 2)) - 21.43
-            return round(ARI, 1)
+            if syllable_count(value) < 3:
+                easy_word.append(value)
+            elif syllable_count(value) > 3:
+                difficult_word.append(value)
+            Number = float((len(easy_word)*1 + len(difficult_word)*3)/float(sc)*100/float(lc))
+            if Number > 20:
+                Number /= 2
+            else:
+                Number = (Number-2)/2
         except Exception as E:
-            print("Error(ARI) : Sentence count is zero, cannot divide")
-            return None
+            print("Error (LWF): ", E)
+    return float(Number)
 
-    def linsear_write_formula(self, text):
-        easy_word = []
-        difficult_word = []
-        text_list = text.split()
+def difficult_words(wordlist):
+    diff_words_set = set()
+    for value in wordlist:
+        if value not in easy_word_set:
+            if syllable_count(value) > 1:
+                if value not in diff_words_set:
+                    diff_words_set.add(value)
+    return len(diff_words_set)
 
-        Number = 0
-        for i, value in enumerate(text_list):
-            if i <= 101:
-                try:
-                    if self.syllable_count(value) < 3:
-                        easy_word.append(value)
-                    elif self.syllable_count(value) > 3:
-                        difficult_word.append(value)
-                    text = ' '.join(text_list[:100])
-                    Number = float((len(easy_word)*1 + len(difficult_word)*3)/self.sentence_count(text))
-                    if Number > 20:
-                        Number /= 2
-                    else:
-                        Number = (Number-2)/2
-                except Exception as E:
-                    print("Error (LWF): ", E)
-        return float(Number)
+def dale_chall_readability_score(lc,dw_c,ASL):
+    if lc > 0:
+        difficult_words = float(dw_c)/lc*100
+    else:
+        print("Error(DCRS): Word Count is zero cannot divide")
+        return None
 
-    def difficult_words(self, text):
-        text_list = text.split()
-        diff_words_set = set()
-        for value in text_list:
-            if value not in easy_word_set:
-                if self.syllable_count(value) > 1:
-                    if value not in diff_words_set:
-                        diff_words_set.add(value)
-        return len(diff_words_set)
+    if difficult_words > 5:
+        score = (0.1579 * difficult_words) + (0.0496 * ASL) + 3.6365
+    else:
+        score = (0.1579 * difficult_words) + (0.0496 * ASL)
+    return round(score, 2)
 
-    def dale_chall_readability_score(self, text):
-        word_count = self.lexicon_count(text)
-        count = word_count - self.difficult_words(text)
-        if word_count > 0:
-            per = float(count)/float(word_count)*100
-        else:
-            print("Error(DCRS): Word Count is zero cannot divide")
-            return None
-        difficult_words = 100-per
-        if difficult_words > 5:
-            score = (0.1579 * difficult_words) + (0.0496 * self.avg_sentence_length(text)) + 3.6365
-        else:
-            score = (0.1579 * difficult_words) + (0.0496 * self.avg_sentence_length(text))
-        return round(score, 2)
+def gunning_fog(dw_c,lc,ASL):
+    try:
+        per_diff_words = (float(dw_c)/lc*100) + 5
+        grade = 0.4*(ASL + per_diff_words)
+        return grade
+    except:
+        print("Error(GF): Word Count is Zero, cannot divide")
 
-    def gunning_fog(self, text):
-        try:
-            per_diff_words = (self.difficult_words(text)/self.lexicon_count(text)*100) + 5
-            grade = 0.4*(self.avg_sentence_length(text) + per_diff_words)
-            return grade
-        except:
-            print("Error(GF): Word Count is Zero, cannot divide")
+def lix(wordlist,ASL,lc):
+    long_words = len([wrd for wrd in wordlist if len(wrd)>6])
+    per_long_words = (long_words) * 100/float(lc)
+    lix = ASL + per_long_words
+    return lix
 
-    def lix(self, text):
-    	words = text.split()
+default_list_of_scores = ["flesch_reading_ease","smog index","flesch reading grade","coleman liau index","automated",
+          "dale chall","linsear write formula","lix","gunning_fog"]
 
-    	words_len = len(words)
-    	long_words = len([wrd for wrd in words if len(wrd)>6])
-    	sentences = self.sentence_count(text)
-
-    	per_long_words = (float(long_words) * 100)/words_len
-    	asl = self.avg_sentence_length(text)
-    	lix = asl + per_long_words
-
-    	return lix 
-
-
-    def text_standard(self, text):
-        grade = []
-
-        # Appending Flesch Kincaid Grade
-        lower = round(self.flesch_kincaid_grade(text))
-        upper = math.ceil(self.flesch_kincaid_grade(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Appending Flesch Reading Easy
-        score = self.flesch_reading_ease(text)
-        if score < 100 and score >= 90:
-            grade.append(5)
-        elif score < 90 and score >= 80:
-            grade.append(6)
-        elif score < 80 and score >= 70:
-            grade.append(7)
-        elif score < 70 and score >= 60:
-            grade.append(8)
-            grade.append(9)
-        elif score < 60 and score >= 50:
-            grade.append(10)
-        elif score < 50 and score >= 40:
-            grade.append(11)
-        elif score < 40 and score >= 30:
-            grade.append(12)
-        else:
-            grade.append(13)
-
-        # Appending SMOG Index
-        lower = round(self.smog_index(text))
-        upper = math.ceil(self.smog_index(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Appending Coleman_Liau_Index
-        lower = round(self.coleman_liau_index(text))
-        upper = math.ceil(self.coleman_liau_index(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Appending Automated_Readability_Index
-        lower = round(self.automated_readability_index(text))
-        upper = math.ceil(self.automated_readability_index(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Appending Dale_Chall_Readability_Score
-        lower = round(self.dale_chall_readability_score(text))
-        upper = math.ceil(self.dale_chall_readability_score(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Appending Linsear_Write_Formula
-        lower = round(self.linsear_write_formula(text))
-        upper = math.ceil(self.linsear_write_formula(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Appending Gunning Fog Index
-        lower = round(self.gunning_fog(text))
-        upper = math.ceil(self.gunning_fog(text))
-        grade.append(int(lower))
-        grade.append(int(upper))
-
-        # Finding the Readability Consensus based upon all the above tests
-        d = dict([(x, grade.count(x)) for x in grade])
-        sorted_x = sorted(d.items(), key=operator.itemgetter(1))
-        final_grade = str((sorted_x)[len(sorted_x)-1])
-        score = final_grade.split(',')[0].strip('(')
-        return str(int(score)-1) + "th " + "and " + str(int(score)) + "th grade"
-
-textstat = textstatistics()
+def list_implemented_scores:
+	print(default_list_of_scores)
+        
+def readability_scores(text,default_scores=default_list_of_scores):
+	wordlist_ = [ch for ch in txt if ch not in exclude]
+	lc = float(len(wordlist_))
+	sc = float(sentence_count(text))
+	syllable_c = float(syllable_count(text))
+	char_c = float(char_count(text))
+	
+	ASL = float(avg_sentence_length(ls,sc))
+	ASW = float(avg_syllables_per_word(syllable_c,lc))
+	ALW = float(avg_letter_per_word(char_c,lc))
+	poly_syllab = polysyllabcount(wordlist_)
+	dw_c = difficult_words(text)
+	
+	score_dict = {}
+	if "flesch reading ease" in default_scores:
+		score_dict["flesch reading ease"] = flesch_reading_ease(ASL,ASW)
+	if "smog index" in default_scores:
+		score_dict["smog index"] = smog_index(poly_syllab,sc)
+	if "flesch reading grade" in default_scores:
+		score_dict["flesch reading grade"] = flesch_reading_ease(ASL,ASW)
+	if "coleman liau index" in default_scores:
+		score_dict["coleman liau index"] = coleman_liau_index(ALW, ASL)
+	if "linsear write formula" in default_scores:
+		score_dict["linsear write formula"] = linsear_write_formula(wordlist_,sc,lc)
+	if "lix" in default_scores:
+		score_dict["lix"] = lix(wordlist_,ASL,lc)
+	if "gunning fog" in default_scores:
+		score_dict["gunning fog index"] = gunning_fog(dw_c,lc,ASL)
+	if "dale chall" in default_scores:
+		score_dict["dale chall"] = dale_chall_readability_score(lc,dw_c,ASL)
+	if "automated" in default_scores:
+		score_dict["automated"] =  automated_readability_index(char_c,lc,sc)
+	
+	return score_dict
+	
+	
+	
+	
